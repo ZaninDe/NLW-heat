a7b82cd9c01300b7225d787e2c625e2506bc4007
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2021-10-30 15:31:44 -0300

    finished

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,53 +1,15 @@
-import { useEffect } from 'react';
+import { useContext } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc'
-import { api } from '../../services/api';
+import { AuthContext } from '../../context/auth';
 
 import styles from './styles.module.scss';
 
-type AuthResponse = {
-  token: string;
-  user: {
-    id: string;
-    avatar_url: string;
-    nome: string;
-    login: string;
-  }
-}
+
 
 export function LoginBox() {
-  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=e2afc54218ea047feb53`;
+  const { signInUrl, user } = useContext(AuthContext);
 
-async function signIn(githubCode: string) {
-  const response = await api.post<AuthResponse>('authenticate', {
-    code: githubCode,
-  })
-
-  const { token, user } = response.data;
-
-  localStorage.setItem('@dowhile:token', token);
-
-  console.log(user);
-}
-
-  // PARTE MUITO IMPORTANTE!!
-  // TRATAMENTO DE VERIFICACAO DE AUTENTICACAO
-
-  useEffect(() => {
-    const url = window.location.href;
-    const hasGithubCode = url.includes('?code=');
-//SEPARANDO O TOKEN DA URL
-    if(hasGithubCode) {
-      const [urlWithoutCode, githubCode] = url.split('?code=');
-
-      console.log({urlWithoutCode, githubCode})
-
-// OCULTANDO O TOKEN DA URL PARA O USER
-      window.history.pushState({}, '', urlWithoutCode);
-
-      signIn(githubCode);
-
-    }
-  }, [])
+  console.log(user)
 
   return (
     <div className={styles.loginBoxWrapper}>
--- web/src/context/auth.tsx
+++ web/src/context/auth.tsx
@@ -1,4 +1,5 @@
-import { createContext, ReactNode } from 'react';
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { api } from '../services/api';
 
 type User = {
   id: string,
@@ -10,20 +11,87 @@ type User = {
 type AuthContextData = {
   user: User | null;
   signInUrl: string;
+  signOut: () => void;
 }
 
 
-const AuthContext = createContext({} as AuthContextData);
+export const AuthContext = createContext({} as AuthContextData);
 
 type AuthProvider = {
   children: ReactNode;
 }
 
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  }
+}
+
 
 
 export function AuthProvider(props: AuthProvider) {
+
+  const [user, setUser] = useState<User | null>(null)
+
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=e2afc54218ea047feb53`;
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>('authenticate', {
+      code: githubCode,
+    })
+  
+    const { token, user } = response.data;
+  
+    localStorage.setItem('@dowhile:token', token);
+
+    api.defaults.headers.common.authorization = `Bearer ${token}`;
+  
+    setUser(user)
+  }
+
+  function signOut() {
+    setUser(null)
+    localStorage.removeItem('@dowhile:token')
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('@dowhile:token')
+
+    if(token) {
+
+      api.defaults.headers.common.authorization = `Bearer ${token}`;
+
+      api.get<User>('profile').then(response => {
+        setUser(response.data)
+      })
+    }
+  }, [])
+  
+    // PARTE MUITO IMPORTANTE!!
+    // TRATAMENTO DE VERIFICACAO DE AUTENTICACAO
+    useEffect(() => {
+      const url = window.location.href;
+      const hasGithubCode = url.includes('?code=');
+  //SEPARANDO O TOKEN DA URL
+      if(hasGithubCode) {
+        const [urlWithoutCode, githubCode] = url.split('?code=');
+  
+        console.log({urlWithoutCode, githubCode})
+  
+  // OCULTANDO O TOKEN DA URL PARA O USER
+        window.history.pushState({}, '', urlWithoutCode);
+  
+        signIn(githubCode);
+  
+      }
+    }, [])
+
   return (
-    <AuthContext.Provider value={null}>
+    <AuthContext.Provider value={{signInUrl, user, signOut}}>
       {props.children}
     </AuthContext.Provider>
   );
